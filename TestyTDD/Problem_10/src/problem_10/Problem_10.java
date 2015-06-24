dc5e17d2f7512b670153bc0bfcefff3abcf98990
slawomirpolak <slawomirpolak93@gmail.com>
2015-06-24 11:21:52 +0200

    TomDoc descriptions for all function

--- TestyTDD/Problem_10/src/problem_10/Problem_10.java
+++ TestyTDD/Problem_10/src/problem_10/Problem_10.java
@@ -1,51 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package problem_10;
 
-/**
- *
- * @author Slawek
- */
+
 public class Problem_10 {
+
+# Public: Static function to write the solution. 
+#
+# #=> 'Liczba wynosi 142913828922'
+# 
+# Returns nothing.
+
     public static void main(String [] args){
-        // create Problem_10 object
+        //zwiekszenie koncowej liczny o 2 
+         System.out.println("Liczba wynosi: " +Solve());
+    }
+	
+# Public: Solve 10th Eulier's problem: Find the sum of all the primes below two million.
+# 	
+# Examples
+#	Solve()
+# #=>142913828922
+#
+
+
+ public static long Solve()
+ {
+    
         Problem_10 prob_10Obj = new Problem_10();
          
-        // answer will be a long
+        //Suma typu Long
         long Sum = 0;
          
-        // start the counter at 1 and increase by 2 (all odd numbers)
+        //  licznik ustawiamy na 1 
         int counter = 1;
          
-        // while counter is less than two million
+        // dopoki licznik bedzie mniejszy niz 2 miliony
         while (counter < 2000000) {
              
-            // checks to see if counter is a prime number - if true - add to Sum
+            // sprawdzenei czy licznik jest liczba pierwsza, jesli tak, dodaj go do sumy
             if (prob_10Obj.isPrime(counter)) {
                 Sum += counter;
             }
-            // increase counter by 2 -> 1, 3, 5, 7, 9, 11, etc.
+            // zwiekszanie licznika o 2
             counter += 2;
         }
-        // add 2 to the Sum - our counter does not count the first prime number -> 2.
-        System.out.println(Sum + 2); 
-    }
+        
+       //zwroc Sume
+        return Sum+2;
+ }
+ 
+ # Public: Check is some number is prime number
+ #
+ # n - the integer number to check
+ #
+ # Examples
+ #
+ #	isPrime(21)
+ #	#=> false
+ #
+ #	isPrime(13)
+ #	#=> true
+ #	
+ 
  
     public boolean isPrime(int n)
         {
+            //sprawdzenie czy n-testowana liczba, wynosi 1 
         if (n == 1){
             return false;
         }
+        //sprawdzenie czy liczba jest pierwsza
         int k = (int) Math.sqrt(n);
             for (int i = 2; i <= k; i++)
             {
+                //jesli n jest podzielna przez i nie jest liczba pierwsza
                 if (n % i == 0)
                     return false;
             }
+            //w innym wypadku zwroc true
             return true;
         }
  
